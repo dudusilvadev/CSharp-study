41c2feb47539ebc1abf9b0f606a75f10bded5c40
DuDu Silva <raimundoeduardo.silva@gmail.com>
2021-12-05 16:57:42 -0300

    Primeiro  commit

---
+++ MyAppStudy/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAppStudy
+{
+
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public Person(string first, string last)
+        {
+            _firstName = first;
+            _lastName = last;
+        }
+
+        public string Name => $"{_firstName} {_lastName}";
+    }
+
+    public class Example
+    {
+        public static void Main()
+        {
+            var person = new Person("Magnus", "Hedlund");
+            Console.WriteLine(person.Name);
+        }
+    }
+}
+// The example displays the following output:
+//      Magnus Hedlund
